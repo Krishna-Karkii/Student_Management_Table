579a19adf5e89c6d7424bb836573469f78250cd1
krishna <krishnakarki1996@gmail.com>
2024-06-11 15:47:25 +0545

    fixed some bugs

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Student Management System")
 
         file_menu = self.menuBar().addMenu("&File")
-        insert_action = QAction("Add")
+        insert_action = QAction("Add", self)
         file_menu.addAction(insert_action)
 
         help_menu = self.menuBar().addMenu("&Help")
-        about_action = QAction("about")
+        about_action = QAction("about", self)
         help_menu.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole)
 
 
 app = QApplication(sys.argv)
