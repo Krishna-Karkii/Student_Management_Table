019a1785575f2500469f661285cdd1ed5d90a3a8
krishna <krishnakarki1996@gmail.com>
2024-06-12 19:10:55 +0545

    remove bugs

--- main.py
+++ main.py
@@ -76,6 +76,12 @@ class MainWindow(QMainWindow):
         delete_button = QPushButton("Delete Cell")
         delete_button.clicked.connect(self.delete)
 
+        children = self.statusbar.findChildren(QPushButton)
+
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+
         self.statusbar.addWidget(edit_button)
         self.statusbar.addWidget(delete_button)
 
